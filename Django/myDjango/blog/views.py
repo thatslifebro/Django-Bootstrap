e2dad970cb8ddf83d5b6c81fded144b800002bef
thatslifebro <null>
2021-08-08 10:42:46 +0000

    CBV로 블로그 상세페이지 만들기

--- Django/myDjango/blog/views.py
+++ Django/myDjango/blog/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+# from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 class PostList(ListView):
     model = Post
     ordering = "-pk"
 
-
+class PostDetail(DetailView):
+    model = Post
 
 
 """"
