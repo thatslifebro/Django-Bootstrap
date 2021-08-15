7e17d1a3b093d0f4aad9629f9527e1767934bcf3
thatslifebro <null>
2021-08-15 05:52:47 +0000

    내비게이션 바, 푸터 모듈화

--- Django/myDjango/blog/tests.py
+++ Django/myDjango/blog/tests.py
@@ -8,6 +8,24 @@ class TestView(TestCase):
     def setUp(self):
         self.client = Client()
     
+    def navbar_test(self, soup):
+        navbar = soup.nav
+        self.assertIn('Blog', navbar.text)
+        self.assertIn('About Me', navbar.text)
+        
+        logo_btn = navbar.find('a', text='Django')
+        self.assertEqual(logo_btn.attrs['href'],'/')
+        
+        home_btn = navbar.find('a', text='Home')
+        self.assertEqual(home_btn.attrs['href'],'/')
+        
+        blog_btn = navbar.find('a', text='Blog')
+        self.assertEqual(blog_btn.attrs['href'],'/blog/')
+        
+        about_me_btn = navbar.find('a', text='About Me')
+        self.assertEqual(about_me_btn.attrs['href'],'/about_me/')
+        
+    
     def test_post_list(self):
         response = self.client.get('/blog/')
         self.assertEqual(response.status_code, 200)
@@ -15,9 +33,7 @@ class TestView(TestCase):
         soup = BeautifulSoup(response.content, 'html.parser')
         self.assertEqual(soup.title.text,'이게 나야')
         
-        navbar = soup.nav
-        self.assertIn('Blog', navbar.text)
-        self.assertIn('About Me', navbar.text)
+        self.navbar_test(soup)
         
         self.assertEqual(Post.objects.count(), 0)
         main_area = soup.find('div', id='main-area')
@@ -56,9 +72,7 @@ class TestView(TestCase):
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, 'html.parser')
         
-        navbar = soup.nav
-        self.assertIn('Blog', navbar.text)
-        self.assertIn('About Me', navbar.text)
+        self.navbar_test(soup)
         
         self.assertIn(post_001.title, soup.title.text)
         
