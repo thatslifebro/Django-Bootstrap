1f1e48fd4c15a56069eb55e3a318bafde46daa2e
thatslifebro <null>
2021-08-22 04:55:10 +0000

    로그인한 사용자만 포스트 작성 페이지 열어주기

--- Django/myDjango/blog/urls.py
+++ Django/myDjango/blog/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path('<int:pk>/', views.PostDetail.as_view()),
     path('category/<str:slug>',views.category_page),
     path('tag/<str:slug>', views.tag_page),
-    # path('<int:pk>/', views.single_post_page),
-    # path('', views.index),  FBV부분
+    path('create_post/', views.PostCreate.as_view()),
 ]
