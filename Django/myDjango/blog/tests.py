b066f550a28cbfffecc30530087f5ecaf4317fa9
thatslifebro <null>
2021-08-28 07:14:51 +0000

    포스트 수정 기능 추가

--- Django/myDjango/blog/tests.py
+++ Django/myDjango/blog/tests.py
@@ -209,3 +209,44 @@ class TestView(TestCase):
         last_post = Post.objects.last()
         self.assertEqual(last_post.title,"Post Form 만들기")
         self.assertEqual(last_post.author.username, "b")
+        
+    def test_update_post(self):
+        update_post_url = f'/blog/update_post/{self.post_003.pk}'
+        
+        response = self.client.get(update_post_url)
+        self.assertNotEqual(response.status_code, 200)
+        
+        self.assertNotEqual(self.post_003.author, self.user_a)
+        self.client.login(
+            username=self.user_a.username,
+            password='aa11aa22'
+        )
+        response = self.client.get(update_post_url)
+        self.assertEqual(response.status_code, 403)
+        
+        self.client.login(
+            username=self.user_b.username,
+            password='bb11bb22'
+        )
+        response = self.client.get(update_post_url)
+        self.assertEqual(response.status_code,200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        
+        self.assertEqual('Edit Post - Blog', soup.title.text)
+        main_area = soup.find('div', id='main-area')
+        self.assertIn('Edit Post', main_area.text)
+        
+        response = self.client.post(
+            update_post_url,
+            {
+                'title': '세 번째 포스트를 수정했습니다. ',
+                'content': '안녕 세계? 우리는 하나!',
+                'category': self.category_me.pk
+            },
+            follow=True
+        )
+        soup = BeautifulSoup(response.content, 'html.parser')
+        main_area = soup.find('div', id='main-area')
+        self.assertIn('세 번째 포스트를 수정했습니다.', main_area.text)
+        self.assertIn('안녕 세계? 우리는 하나!', main_area.text)
+        self.assertIn(self.category_me.name, main_area.text)
