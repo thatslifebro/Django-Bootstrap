945e1fa177711d93e792378f7bed33a6b8b7cfc4
thatslifebro <sy980407@gmail.com>
2021-10-03 01:11:19 +0000

    댓글 나타내기

--- Django/myDjango/blog/admin.py
+++ Django/myDjango/blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from markdownx.admin import MarkdownxModelAdmin
-from .models import Post, Category, Tag
+from .models import Post, Category, Tag, Comment
 
 #Register your models here.
 admin.site.register(Post, MarkdownxModelAdmin)
@@ -13,3 +13,4 @@ class TagAdmin(admin.ModelAdmin):
     
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Tag, TagAdmin)
+admin.site.register(Comment)
