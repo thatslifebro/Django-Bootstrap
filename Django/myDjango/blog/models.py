8263d144afd437c31d064c7325c0deb200be9d5d
thatslifebro <sy980407@gmail.com>
2021-10-03 09:42:30 +0000

    대문페이지 수정

--- Django/myDjango/blog/models.py
+++ Django/myDjango/blog/models.py
@@ -63,6 +63,14 @@ class Post(models.Model):
     def get_content_markdown(self):
         return markdown(self.content)
     
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return 'https://doitdjango.com/avatar/id/323/079e8f6287509f34/svg/{self.author.email}'
+    
+    
+    
     
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
