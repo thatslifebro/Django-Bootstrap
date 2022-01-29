029161161684dd8ad7801a1009486af66032c69a
thatslifebro <75624064+thatslifebro@users.noreply.github.com>
2022-01-29 16:58:59 +0900

    바꾸는중
    박스만드는중

--- Django/myDjango/single_pages/views.py
+++ Django/myDjango/single_pages/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
-from blog.models import Post
+from blog.models import Post, Category
 
 # Create your views here.
 def landing(request):
     recent_posts = Post.objects.order_by('-pk')[:3]
+    catagories = Category.name
     return render(
         request,
         'single_pages/landing.html',
         {
             'recent_posts': recent_posts,
+            'categories': categories,
         }
     )
 
